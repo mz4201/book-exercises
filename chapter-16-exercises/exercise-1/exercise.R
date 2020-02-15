7a4a89351f72eb4a01d2d7fd7d5fcf14f4cbdefa
mz4201 <47264851+mz4201@users.noreply.github.com>
2020-02-14 21:54:47 -0800

    completed

--- chapter-16-exercises/exercise-1/exercise.R
+++ chapter-16-exercises/exercise-1/exercise.R
@@ -2,7 +2,7 @@
 
 # Install and load the `ggplot2` package
 # You will also want to load `dplyr`
-install.packages("ggplot2")
+#install.packages("ggplot2")
 library(ggplot2)
 library(dplyr)
 # For this exercise you'll be working with the `diamonds` data set included in 
